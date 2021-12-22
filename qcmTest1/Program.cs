62d0bca714ce981915c8c0b57be0b6a0119ff13d
Alek Mikolajek <alekmikopro@gmail.com>
2021-12-22 14:28:21 +0100

    1er jet, base du progamme

---
+++ qcmTest1/Program.cs
@@ -0,0 +1,53 @@
+﻿//acceuil de l'utilisateur. au - 2 QCM
+Console.WriteLine("Bonjour, bienvenue sur le test Alek !\nDes questions sur le thème " +
+    "du cinéma vous serrons posez." +
+    "\nPour rappel un QCM est un Questionnaire à choix multiples. \nVous aurez le " +
+    "choix de la réponse parmis 4 propositions de réponse." +
+    "\nTaper \"Entrer\" pour commencer le questionnaire. ");
+//appuyer sur Entrée pour commencer, rappel de ce qu'est une QCM
+Console.ReadLine();
+//Première question
+//Variables première question
+string pinocchio = "Pinocchio";
+string laBoiteAMusique = "La Boîte à musique";
+string laPetiteSirene = "La Petite Sirène";
+string blancheNeige = "Blanche - Neige et les Sept Nains";
+
+Console.WriteLine("Quel est le plus vieux film des Studios Dysney ?" +
+    "\n1 : " + pinocchio +
+    "\n2 : " + laBoiteAMusique +
+    "\n3 : " + laPetiteSirene +
+    "\n4 : " + blancheNeige +
+    "\n\nTaper le numéro associé à la réponse (\"1\" ou \"2\" ou \"3\" ou \"4\")"
+);
+string rightAnswerQuestion1 = Console.ReadLine();
+//Associer la réponse à une variable
+Console.WriteLine("Votre réponse : ");
+//Deuxième question
+
+
+//Troisième question
+
+
+//Quatrième question
+
+
+//Cinquième question
+
+
+//Sixième question
+
+
+//Septième question
+
+
+//Huitième question
+
+
+//Neuvième question
+
+
+//Dixième question
+
+
+//Fin de la partie, afficher les statistiques (nbr de réponses bonnes)
